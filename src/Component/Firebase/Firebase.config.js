e383fdddcb778829be5c581d086e18e3a5820fa8
fuhad1998 <fuhadrare444@gmail.com>
2022-04-04 23:10:15 +0600

    add env file

--- src/Component/Firebase/Firebase.config.js
+++ src/Component/Firebase/Firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBHM0TWLvDG20PvlzQfdm8rE6cR2NqpNWI",
-    authDomain: "bangla-drones.firebaseapp.com",
-    projectId: "bangla-drones",
-    storageBucket: "bangla-drones.appspot.com",
-    messagingSenderId: "400692985780",
-    appId: "1:400692985780:web:ae74d54b92f8caaec8fce1",
-    measurementId: "G-5G20JJ7RLD"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
   };
   export default firebaseConfig;

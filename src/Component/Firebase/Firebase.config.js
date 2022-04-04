162c2777d5a293f89d1723199fc434491e1e47a4
fuhad1998 <fuhadrare444@gmail.com>
2022-04-04 15:44:05 +0600

    added firebase

---
+++ src/Component/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHM0TWLvDG20PvlzQfdm8rE6cR2NqpNWI",
+    authDomain: "bangla-drones.firebaseapp.com",
+    projectId: "bangla-drones",
+    storageBucket: "bangla-drones.appspot.com",
+    messagingSenderId: "400692985780",
+    appId: "1:400692985780:web:ae74d54b92f8caaec8fce1",
+    measurementId: "G-5G20JJ7RLD"
+  };
+  export default firebaseConfig;
--- src/Component/Homepage/Header/Header.js
+++ src/Component/Homepage/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand to="/home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand to="/home" >Bangla Drones</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">

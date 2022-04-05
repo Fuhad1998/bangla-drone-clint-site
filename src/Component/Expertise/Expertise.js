3bd642d5aa8378b9adb2d71b7e76c57f6a9ea4d7
fuhad1998 <fuhadrare444@gmail.com>
2022-04-05 23:33:18 +0600

    add img

--- src/Component/Expertise/Expertise.js
+++ src/Component/Expertise/Expertise.js
@@ -1,10 +1,11 @@
 import React from "react";
-import img from "../../Drone-img/young-man-operating-flying-drone-260nw-684125701.jpg";
+
+import './Expertise.css'
 
 const Expertise = () => {
   return (
-    <div className="row bg-dark">
-      <div className="col-xs-12 col-sm-12 col-md-6 ">
+    <div className="row experts-container ">
+      <div className="col-xs-12 col-sm-12 col-md-6 bg-dark ">
         <h1 className="text-white pt-5">Our Expertise</h1>
         <h1 className="text-white">WE ARE PROFESSIONAL IN THE FIELD AERIAL & DRONE</h1>
         <h4 className="text-white">COMMERCIAL VIDEOGRAPHY</h4>
@@ -15,8 +16,8 @@ const Expertise = () => {
          <h6 className="text-white">adipisicing elit. Nisi itaque quibusdam tenetur</h6>
          <h6 className="text-white"> sit consectetur voluptatem assumenda voluptas</h6>
       </div>
-      <div className="col-xs-12 col-sm-12 col-md-6">
-        <img style={{height: "600px", width: "660px"}} src={img} alt="" />
+      <div className="col-xs-12 col-sm-12 col-md-6 " style={{height: "400px"}}>
+        
       
       </div>
     </div>

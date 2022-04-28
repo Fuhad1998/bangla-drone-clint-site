87a01847c0719e36cf5d12a7f63f80b7b2a1eeb3
fuhad1998 <fuhadrare444@gmail.com>
2022-04-29 02:47:57 +0600

    add style

--- src/Component/Homepage/Service/Service.js
+++ src/Component/Homepage/Service/Service.js
@@ -7,22 +7,22 @@ const Service = ({ service }) => {
   const { name, detail, img, price, model, id } = service;
   return (
     <Col xs={12} sm={6} md={4} className="g-4 ">
-      <Card className="h-100 p-3 shadow px-5">
-        <Card.Img variant="top" src={img} />
-        <Card.Body>
-          <Card.Title>Name: {name}</Card.Title>
-          <Card.Title>
-            Price BD:$ <span className="text-success">{price}</span>
+      <Card className="h-100  shadow ">
+        <Card.Img variant="top" className="px-5" src={img} />
+        <Card.Body className="bg-secondary bg-opacity-10">
+          <Card.Title className="">Name: {name}</Card.Title>
+          <Card.Title className="">
+            Price BD:$ <span className="text-">{price}</span>
           </Card.Title>
-          <Card.Title>
-            Model: <span className="text-success">{model}</span>
+          <Card.Title className="">
+            Model: <span className="text- ">{model}</span>
           </Card.Title>
-          <Card.Title>
+          <Card.Title className="">
             Detail:<span>{detail}</span>
             <br />
           </Card.Title>
-          <button type="button" className="btn-primary service-btn">
-            <Link className="btn-primary service-btn link-btn" to={`/purchase/${id}`}>Buy Now</Link>
+          <button type="button" className="btn-primary service-btn p-2 px-4">
+            <Link className="btn-primary  link-btn" to={`/purchase/${id}`}>Buy Now</Link>
           </button>
         </Card.Body>
       </Card>
